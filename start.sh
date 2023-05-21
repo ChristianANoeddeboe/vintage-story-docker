1f06142107a5692d1e2390f2df2f6ac2e2d2a56d
Christian <angercraft@gmail.com>
2023-05-21 13:54:16 +0200

    version and addec cleanup method to properly stop the server when stopping the container

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+cleanup() {
+    echo "Container stopped, performing cleanup..."
+    /var/vintagestory/server/server.sh stop
+}
+
+# Trap SIGTERM
+trap 'cleanup' SIGTERM
+
 # Start the server
-/var/vintagestory/server/server.sh start && tail -f /var/vintagestory/data/Logs/server-main.txt
+/var/vintagestory/server/server.sh start && tail -f /var/vintagestory/data/Logs/server-main.txt &
+# Wait
+wait $!
